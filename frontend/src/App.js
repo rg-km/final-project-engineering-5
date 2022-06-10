fc6fd6f4e236c83d59c5775a172193a5638e1064
Saleh Zaidan <salehzaidan64@gmail.com>
2022-06-10 22:56:24 +0700

    Add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <Navigation />
-      <main>
+      <main className="grow">
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
 

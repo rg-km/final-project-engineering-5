b8f546076d735ae8a04fccc786ca7c3fb9adb6c8
Saleh Zaidan <salehzaidan64@gmail.com>
2022-06-17 08:47:12 +0700

    Create login page layout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import RegisterPage from './layout/RegisterPage';
+import LoginPage from './layout/LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +14,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<h2>Landing Page</h2>} />
-          <Route path="login" element={<h2>Login Page</h2>} />
+          <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="beasiswa" element={<h2>Beasiswa Page</h2>} />
         </Route>

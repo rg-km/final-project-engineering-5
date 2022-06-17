661b436ef115f40287472f271c716be4dbc390fd
Saleh Zaidan <salehzaidan64@gmail.com>
2022-06-17 16:37:12 +0700

    Create beasiswa list and detail component

---
+++ frontend/src/layout/BeasiswaListPage.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import BeasiswaDetail from '../components/BeasiswaDetail';
+import BeasiswaList from '../components/BeasiswaList';
+import { getBeasiswaList } from '../lib/beasiswa';
+
+function BeasiswaListPage() {
+  const [beasiswaList, setBeasiswaList] = useState(null);
+  const [activeBeasiswa, setActiveBeasiswa] = useState(null);
+
+  useEffect(() => {
+    getBeasiswaList().then((d) => {
+      setBeasiswaList(d.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {!beasiswaList ? (
+        <p>Loading</p>
+      ) : (
+        <div className="mx-auto grid max-w-screen-lg grid-cols-[1fr_2fr] gap-4 py-16 px-4">
+          <BeasiswaList
+            beasiswaList={beasiswaList}
+            activeBeasiswa={activeBeasiswa}
+            setActiveBeasiswa={setActiveBeasiswa}
+          />
+          <BeasiswaDetail beasiswa={activeBeasiswa} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default BeasiswaListPage;

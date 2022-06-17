b377747621f1d30bf8e72221a3550b71a9dce54b
Saleh Zaidan <salehzaidan64@gmail.com>
2022-06-17 09:21:35 +0700

    Add cors

--- dummy-server/index.js
+++ dummy-server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import apiRoutes from './routes/index.js';
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', apiRoutes);
 

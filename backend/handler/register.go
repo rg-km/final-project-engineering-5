a84eca58ea089ad38ffb81a41d4b7c45fbccce73
sinulingga23 <sinulinggatwo@gmail.com>
2022-06-20 14:30:48 +0700

    feat: restore deleted handler and endpoints

--- backend/handler/register.go
+++ backend/handler/register.go
@@ -15,6 +15,7 @@ func (h *handler) registerHandler(r *gin.Engine) {
 
 	baseEndpoints.POST("/siswa/login", h.handleLoginSiswa)
 	baseEndpoints.GET("/siswa", h.handleGetListSiswa)
+	baseEndpoints.POST("/siswa/signup", h.handleRegisterSiswa)
 	
 	baseEndpoints.POST("/mitra/login", h.handleLoginMitra)
 	baseEndpoints.POST("/mitra/signup", h.handleRegisterMitra)
@@ -287,6 +288,37 @@ func (h *handler) handleUpdateBeasiswa(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 	return
 }
+
+func (h *handler) handleRegisterSiswa(c *gin.Context) {
+	request := payload.Siswa{}
+	if err := c.Bind(&request); err != nil {
+		c.JSON(http.StatusBadRequest, struct {
+			Message string `json:"message"`
+			Error   string `json:"error"`
+		}{Message: err.Error(), Error: utility.ErrBadRequest.Error()})
+		return
+	}
+
+	response, err := h.siswaService.RegisterSiswa(request)
+	if err != nil {
+		if err == utility.ErrNoDataFound {
+			c.JSON(http.StatusNotFound, struct {
+				Message string `json:"message"`
+				Error   string `json:"error"`
+			}{Message: "Tidak dapat melayani permintaan anda saat ini.", Error: err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, struct {
+			Message string `json:"message"`
+			Error   string `json:"error"`
+		}{Message: "Tidak dapat melayani permintaan anda saat ini.", Error: err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, response)
+	return
+}
+
 func (h *handler) handleRegisterMitra(c *gin.Context) {
 	request := payload.MitraDetail{}
 	if err := c.Bind(&request); err != nil {

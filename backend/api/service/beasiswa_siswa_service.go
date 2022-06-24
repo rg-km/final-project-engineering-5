25e925a6b45b482dfbb0ab48c759da0608e0f180
sinulingga23 <sinulinggatwo@gmail.com>
2022-06-24 09:36:22 +0700

    feat: change api get list beasiswa siswa

--- backend/api/service/beasiswa_siswa_service.go
+++ backend/api/service/beasiswa_siswa_service.go
@@ -7,4 +7,5 @@ import (
 type BeasiswaSiswaService interface {
 	UpdateStatusBeasiswa(request payload.BeasiswaSiswaStatusUpdateRequest, id int) (*payload.BeasiswaSiswaStatusUpdateResponse, error)
 	ApplyBeasiswa(request payload.BeasiswaSiswaApplyRequest) (*payload.BeasiswaSiswaApplyResponse, error)
+	GetListBeassiwaSiswaByMitraId(request payload.ListBeasiswaSiswaByMitraIdRequest) (*payload.ListBeasiswaSiswaByMitraIdResponse, error)
 }

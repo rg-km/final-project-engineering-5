8dffe3526352bba02c67e19d692129911d90c0d1
Hatta56 <hattasugiarto443@gmail.com>
2022-06-25 19:43:46 +0700

    add:idsiswa on respon login

--- backend/payload/token.go
+++ backend/payload/token.go
@@ -3,21 +3,23 @@ package payload
 import (
 	"github.com/golang-jwt/jwt/v4"
 )
+
 type LoginRequest struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
 type LoginResponse struct {
-	Email string `json:"email"`
-	Role string `json:"role"`
-	IdUser int `json:"idUser"`
-	Token 	string `json:"token"`
+	Email   string `json:"email"`
+	Role    string `json:"role"`
+	IdUser  int    `json:"idUser"`
+	Token   string `json:"token"`
+	IdSiswa int    `json:"idSiswa"`
 }
 
 type Claims struct {
-	Email string
-	Role string
+	Email  string
+	Role   string
 	IdUser int
 	jwt.StandardClaims
 }

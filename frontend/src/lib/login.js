e5245c85e692d62a613a45bbca93f455f0c82b56
Saleh Zaidan <salehzaidan64@gmail.com>
2022-06-25 18:09:18 +0700

    Add login error handling

--- frontend/src/lib/login.js
+++ frontend/src/lib/login.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
 
 export async function login(role, email, password) {
-  try {
-    const response = await axios.post(
-      `${process.env.REACT_APP_API_BASE_URL}/api/${role.toLowerCase()}/login`,
-      {},
-      { auth: { username: email, password } }
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await axios.post(
+    `${process.env.REACT_APP_API_BASE_URL}/api/${role.toLowerCase()}/login`,
+    {},
+    { auth: { username: email, password } }
+  );
+  return response.data;
 }
